8bc3dc0c0fc0bab7f9ab731cce3351c501802d96
Anisha0209 <divyaanisha.it@gmail.com>
2024-11-22 16:15:34 +0800

    Updated with list of visited places and removing the unmarked place if unmark button is clicked. Also updated the MOCK API URL

--- src/components/HistoricalPlaceList.js
+++ src/components/HistoricalPlaceList.js
@@ -9,7 +9,9 @@ const HistoricalPlaceList = () => {
   const places = useSelector((state) => state.places);
   const dispatch = useDispatch();
   const [randomPlace, setRandomPlace] = useState(null);
+  const [visitedPlaces, setVisitedPlaces] = useState([]);
 
+  // Get random suggestion
   const getRandomSuggestion = () => {
     if (places.length > 0) {
       const randomIndex = Math.floor(Math.random() * places.length);
@@ -17,12 +19,38 @@ const HistoricalPlaceList = () => {
     }
   };
 
+  // Filter and show visited places
+  const showVisited = () => {
+    const visited = places.filter((place) => place.visited); // Filter places that are visited
+    setVisitedPlaces(visited); // Set visited places to state
+  };
+
+  // Show all places when "Show All" is clicked
+  const showAll = () => {
+    setVisitedPlaces([]); // Reset the visitedPlaces state to show all places
+  };
+
+  // Handle unmarking a place as visited
+  const handleUnmarkVisited = (placeId) => {
+    // Dispatch unmark action
+    dispatch(unmarkAsVisited(placeId));
+
+    // Update visitedPlaces state after unmarking
+    setVisitedPlaces((prevVisitedPlaces) =>
+      prevVisitedPlaces.filter((place) => place.id !== placeId)
+    );
+  };
+
   return (
     <div className="historical-places-container">
       <h2 className="place-title">List of Historical Places</h2>
+      <button onClick={showVisited}>List of Visited Places</button>
+      <button onClick={showAll}>List of All Places</button>
+
+      {/* Conditionally render either visited places or all places */}
       <div className="places-list-container">
         <ul>
-          {places.map((place) => (
+          {(visitedPlaces.length > 0 ? visitedPlaces : places).map((place) => (
             <li key={place.id} className="place-item">
               <img src={place.image} alt={place.name} width="250" />
               <div className="place-info">
@@ -33,7 +61,7 @@ const HistoricalPlaceList = () => {
                   <button onClick={() => dispatch(markAsVisited(place.id))}>
                     Mark as Visited
                   </button>
-                  <button onClick={() => dispatch(unmarkAsVisited(place.id))}>
+                  <button onClick={() => handleUnmarkVisited(place.id)}>
                     Unmark as Visited
                   </button>
                   {place.visited && (
@@ -46,6 +74,7 @@ const HistoricalPlaceList = () => {
         </ul>
       </div>
 
+      {/* Random suggestion */}
       <div className="random-suggestion-container">
         <button onClick={getRandomSuggestion}>Get a Random Suggestion</button>
         {randomPlace && <RandomSuggestion currentPlaceId={randomPlace.id} />}
